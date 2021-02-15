9499697e9403b91f6016cc423e2286478c6b3d64
RicardoSoto23 <A01423121@itesm.mx>
2021-02-14 22:30:00 -0600

    finished module 11

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React from "react";
 import { Route, Switch } from "react-router-dom";
-import HomePage from './home/HomePage';
-import AboutPage from './about/AboutPage';
-import Header from './common/Header';
-import PageNotFound from './PageNotFound';
-import CoursesPage from './courses/CoursesPage';
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 function App() {
-    return (
-        <div className="container-fluid">
-            <Header />
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/about" component={AboutPage} />
-                <Route path="/courses" component={CoursesPage} />
-                <Route component={PageNotFound} />
-            </Switch>
-        </div>
-    );
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course/" component={ManageCoursePage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { loadCourses, saveCourse } from "../../redux/actions/courseActions";
+import { loadAuthors } from "../../redux/actions/authorActions";
+import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
+
+// props will have any properties not de-structured on the left
+function ManageCoursePage({
+  courses,
+  authors,
+  loadAuthors,
+  saveCourse,
+  loadCourses,
+  history,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    if (courses.length === 0) {
+      loadCourses().catch((error) => {
+        alert("Loading courses failed" + error);
+      });
+    } else {
+      setCourse({ ...props.course });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed" + error);
+      });
+    }
+  }, [props.course]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCourse((prevCourse) => ({
+      ...prevCourse,
+      [name]: name === "authorId" ? parseInt(value, 10) : value,
+    }));
+  }
+
+  function handleSave(event) {
+    event.preventDefault();
+    saveCourse(course).then(() => {
+      history.push("/courses");
+    });
+  }
+
+  return (
+    <CourseForm
+      course={course}
+      errors={errors}
+      authors={authors}
+      onChange={handleChange}
+      onSave={handleSave}
+    />
+  );
+}
+
+ManageCoursePage.propTypes = {
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired,
+  courses: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired,
+  saveCourse: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+};
+
+// connect function connects our components to redux
+// Calling connect returns a function and that function calls our component
+// export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
+
+//or this (below)
+
+export function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug) || null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
+  return {
+    course,
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+const mapDispatchToProps = {
+  // loadCourses: loadCourses,
+  //loadAuthors: loadAuthors,
+  // Object shorthand syntax, the right hand side is removable since it matches the left hand side
+  loadCourses,
+  loadAuthors,
+  saveCourse,
+};
+
+const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+export default connectedStateAndProps(ManageCoursePage);
